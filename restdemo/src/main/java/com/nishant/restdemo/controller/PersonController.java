9f2b2d4312b2a71c3ae607ad3f7a6029a1a4e5d2
Nikhil <nikhil.n@evolvussolutions.com>
2021-07-08 18:27:35 +0530

    minor changes

--- restdemo/src/main/java/com/nishant/restdemo/controller/PersonController.java
+++ restdemo/src/main/java/com/nishant/restdemo/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.nishant.restdemo.model.Person;
+
 
 @RestController
 public class PersonController {
@@ -28,11 +30,13 @@ public class PersonController {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		HttpEntity<Person> entity = new HttpEntity<Person>(person, headers);
+		
+		HttpEntity<Person> entity = new HttpEntity<>(person, headers);
 		System.out.println(person);
 		
-		return restTemplate.exchange(
-		         "https://serene-spire-67366.herokuapp.com/rest/postaccout-json", HttpMethod.POST, entity, String.class).getBody();
+		ResponseEntity<String> resp = restTemplate.exchange(
+		         "https://serene-spire-67366.herokuapp.com/rest/postaccout-json", HttpMethod.POST, entity, String.class);
+		return resp.getBody();
 	}
 
 }
